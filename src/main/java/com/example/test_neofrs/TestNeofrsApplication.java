64a806bda25eea1f7e9a1410fec3eba28d9bcd1f
randy <tmps880305@gmail.com>
2019-01-16 10:53:04 +0800

    file

---
+++ src/main/java/com/example/test_neofrs/TestNeofrsApplication.java
@@ -0,0 +1,19 @@
+package com.example.test_neofrs;
+
+import com.example.test_neofrs.testConnect.SpringRestTemplateApp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNeofrsApplication {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(TestNeofrsApplication.class, args);
+//    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringRestTemplateApp.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/example/test_neofrs/testConnect/Member.java
@@ -0,0 +1,41 @@
+package com.example.test_neofrs.testConnect;
+
+public class Member {
+    private Integer id;
+    private String name;
+    private String department;
+
+    public Member() {
+    }
+
+    public Member(Integer id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+}
